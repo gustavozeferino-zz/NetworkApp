035c02db8d1120e4b1c6a8c1e8ae04f6893f35d7
Jolanda <jolanda.kossakowski@gmail.com>
2015-09-08 17:55:45 +0200

    update
    updates R-code for running app in R

--- runapp.R
+++ runapp.R
@@ -16,4 +16,34 @@ if(!"huge" %in% installed.packages())
 }
 library(huge)
 
+if(!"qgraph" %in% installed.packages())
+{
+  install.packages("qgraph")
+}
+library(qgraph)
+
+if(!"igraph" %in% installed.packages())
+{
+  install.packages("igraph")
+}
+library(igraph)
+
+if(!"pcalg" %in% installed.packages())
+{
+  install.packages("pcalg")
+}
+library(pcalg)
+
+if(!"psych" %in% installed.packages())
+{
+  install.packages("psych")
+}
+library(psych)
+
+if(!"ggplot2" %in% installed.packages())
+{
+  install.packages("ggplot2")
+}
+library(ggplot2)
+
 runGitHub("NetworkApp", "JolandaKossakowski") 
